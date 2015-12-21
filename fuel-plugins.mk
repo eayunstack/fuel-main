1479ce1bf09dd4e40ca0c0e2416df7f539730ce0
Zhao Chao <zhaochao1984@gmail.com>
2015-12-21 15:56:51 +0800

    remove eqlx plugin from EayunStack 1.0.1.
    Signed-off-by: Zhao Chao <zhaochao1984@gmail.com>

--- fuel-plugins.mk
+++ fuel-plugins.mk
@@ -1,12 +1,10 @@
 .PHNOY: fuel-plugins
 
-FP_EQLX_REPO?=https://github.com/eayunstack/fuel-plugin-cinder-eqlx.git
 FP_FWAAS_REPO?=https://github.com/eayunstack/fuel-plugin-neutron-fwaas.git
 FP_LBAAS_REPO?=https://github.com/eayunstack/fuel-plugin-neutron-lbaas.git
 FP_VPNAAS_REPO?=https://github.com/eayunstack/fuel-plugin-neutron-vpnaas.git
 FP_QOS_REPO?=https://github.com/eayunstack/fuel-plugin-neutron-qos.git
 
-FP_EQLX_COMMIT?=HEAD
 FP_FWAAS_COMMIT?=HEAD
 FP_LBAAS_COMMIT?=HEAD
 FP_VPNAAS_COMMIT?=HEAD
@@ -31,12 +29,10 @@ $(BUILD_DIR)/fuel-plugins/$1.done: \
 endef
 
 ifeq ($(EAYUNSTACK_BUILD),true)
-$(eval $(call build_repo,fuel-plugin-cinder-eqlx,$(FP_EQLX_REPO),$(FP_EQLX_COMMIT),none,none))
 $(eval $(call build_repo,fuel-plugin-neutron-fwaas,$(FP_FWAAS_REPO),$(FP_FWAAS_COMMIT),none,none))
 $(eval $(call build_repo,fuel-plugin-neutron-lbaas,$(FP_LBAAS_REPO),$(FP_LBAAS_COMMIT),none,none))
 $(eval $(call build_repo,fuel-plugin-neutron-vpnaas,$(FP_VPNAAS_REPO),$(FP_VPNAAS_COMMIT),none,none))
 $(eval $(call build_repo,fuel-plugin-neutron-qos,$(FP_QOS_REPO),$(FP_QOS_COMMIT),none,none))
-$(eval $(call build_fuel_plugin,fuel-plugin-cinder-eqlx))
 $(eval $(call build_fuel_plugin,fuel-plugin-neutron-fwaas))
 $(eval $(call build_fuel_plugin,fuel-plugin-neutron-lbaas))
 $(eval $(call build_fuel_plugin,fuel-plugin-neutron-vpnaas))
